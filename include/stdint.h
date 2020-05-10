5187ac03be895c9915acb4a3dcb078f1296dd9ab
Norman Liu <normanwuweiliu@gmail.com>
2020-05-10 02:04:53 -0700

    Added template for stdint.h

---
+++ include/stdint.h
@@ -0,0 +1,18 @@
+#ifndef _STDINT_H
+#define _STDINT_H 1
+ 
+#include <sys/cdefs.h>
+ 
+#define EOF (-1)
+ 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* typedefs go here*/
+ 
+#ifdef __cplusplus
+}
+#endif
+ 
+#endif
